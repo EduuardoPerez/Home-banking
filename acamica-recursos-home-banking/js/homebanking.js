0c5f6285e24459d19aef7a25d25ba1038a91256f
Eduardo Perez <edujpp1@gmail.com>
2019-05-10 16:34:30 -0300

    Se implementaron las funciones sumarSaldo, restarSaldo, cambiarLimiteDeExtraccion, depositarDinero y se están realizando las validaciones para terminar de implementar la funcion extraerDinero

--- acamica-recursos-home-banking/js/homebanking.js
+++ acamica-recursos-home-banking/js/homebanking.js
@@ -1,4 +1,8 @@
 //Declaración de variables
+var nombreUsuario = 'Eduardo Perez'
+var saldoCuenta = 3800
+var limiteExtraccion = 15000
+
 
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML.
 window.onload = function() {
@@ -8,17 +12,54 @@ window.onload = function() {
 }
 
 
+//Funciones que realizan operaciones necesarias
+function sumarSaldo(dinero){
+	saldoCuenta += dinero
+}
+
+function restarSaldo(dinero){
+	saldoCuenta -= dinero
+}
+
+
 //Funciones que tenes que completar
 function cambiarLimiteDeExtraccion() {
-
+	var nuevolimiteExtraccion = parseInt(prompt('Ingresar el nuevo limite de extracción'))
+	limiteExtraccion = nuevolimiteExtraccion
+	actualizarLimiteEnPantalla()
+	alert('Su nuevo limite de extracción es: '+limiteExtraccion)
 }
 
 function extraerDinero() {
-
+	var dineroAExtraer = parseInt(prompt('Ingresar el monto a retirar'))
+	if(dineroAExtraer<=saldoCuenta){
+		if (dineroAExtraer<=limiteExtraccion){
+			if(true){ // Como el Home Banking maneja solo billetes de 100, verificar
+						// que el monto que se quiere extraer pueda ser entregado
+						// solo con esos billetes.
+				var saldoAnterior = saldoCuenta
+				restarSaldo(dineroAExtraer)
+				actualizarSaldoEnPantalla()
+				alert('Ha retirado: '+dineroAExtraer+'\n'+
+							'Saldo anterior: '+saldoAnterior+'\n'+
+							'Saldo actual: '+saldoCuenta)
+			}
+		} else {
+			alert('No puede extraer un monto mayor al limite de extraccion')
+		}
+	} else {
+		alert('No puede extraer más del saldo disponible')
+	}
 }
 
 function depositarDinero() {
-
+	var dineroADepositar = parseInt(prompt('Ingresar el monto a depositar'))
+	var saldoAnterior = saldoCuenta
+	sumarSaldo(dineroADepositar)
+	actualizarSaldoEnPantalla()
+	alert('Ha depositado: '+dineroADepositar+'\n'+
+				'Saldo anterior: '+saldoAnterior+'\n'+
+				'Saldo actual: '+saldoCuenta)
 }
 
 function pagarServicio() {
